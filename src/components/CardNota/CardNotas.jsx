15682b0532e7e33ba21ebdb50178f04b263fbd28
CaioKeidi <caiokeidi@gmail.com>
2021-02-15 14:25:08 -0300

    add: Delete Notes function

--- src/components/CardNota/CardNotas.jsx
+++ src/components/CardNota/CardNotas.jsx
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
-import "./estilo.css"
+import "./estilo.css";
+import {ReactComponent as DeleteSVG} from "../../assets/img/delete.svg";
 
 class CardNotas extends Component {
-    state = {  }
+    apagar(){
+        const indice = this.props.indice;
+        this.props.apagarNota(indice);
+    }
     render() { 
         return ( 
                 <section className='card-nota'>
                     <header className='card-nota-cabecalho'>
                         <h3 className='card-nota-titulo'>{this.props.titulo}</h3>
+                        <DeleteSVG onClick={this.apagar.bind(this)}/>
                     </header>
                     <p className='card-nota-texto'>{this.props.texto}</p>
                 </section>
